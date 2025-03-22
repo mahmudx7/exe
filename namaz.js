f9f8248e36ccbe3b9682158d29b7dba6ab14eda8
Mah MUD <mahmudx077@gmail.com>
2025-03-22 07:05:24 +0600

    Update namaz.js

--- namaz.js
+++ namaz.js
@@ -1,5 +1,10 @@
 const axios = require("axios");
 
+const baseApiUrl = async () => {
+  const base = 'https://mahmud-namaz.onrender.com';
+  return base;
+};
+
 module.exports = {
   config: {
     name: "namaz",
@@ -9,12 +14,12 @@ module.exports = {
     countDown: 5,
     role: 0,
     category: "Islamic",
-    guide: "{pn} <city>\n\n- {pn}: Command prefix (e.g., ! or ?)\n- <city>: Name of the city for which you want the prayer times. If no city is provided, the default city is 'Dhaka'. Example: `{pn} Dhaka`"
+    guide: "{pn} <city>\n\n- {pn}: <city> Example: {pn} Dhaka"
   },
 
   onStart: async function ({ message, args }) {
     const city = args.join(" ") || "Dhaka";
-    const apiUrl = `https://mahmud-namaz.onrender.com/font3/${encodeURIComponent(city)}`;
+    const apiUrl = `${await baseApiUrl()}/font3/${encodeURIComponent(city)}`;
 
     try {
       const response = await axios.get(apiUrl);
