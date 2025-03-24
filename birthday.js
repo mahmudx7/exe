c413547ec5f8ae60c7243f891645a8b61199d626
Mah MUD <mahmudx077@gmail.com>
2025-03-24 17:37:24 +0600

    Update birthday.js

--- birthday.js
+++ birthday.js
@@ -4,62 +4,29 @@ module.exports = {
     aliases: ["wish"],
     version: "1.7",
     role: 0,
-    author: "Mahmud",
+    author: "MahMUD",
     category: "love",
     countDown: 5,
-    guide: {
-      en: "{p}{n} @mention",
-    },
+    guide: { en: "{p}{n} @mention" },
   },
 
   onStart: async function ({ api, event }) {
     const mention = Object.keys(event.mentions);
-
     if (mention.length === 0) {
-      return api.sendMessage(
-        "❌ You need to tag someone to wish!",
-        event.threadID,
-        event.messageID
-      );
+      return api.sendMessage("❌ You need to tag someone to wish!", event.threadID, event.messageID);
     }
 
-    const taggedUserID = mention[0];
-    const taggedUserName = event.mentions[taggedUserID];
-
+    const taggedUserName = event.mentions[mention[0]].replace('@', '');
     try {
-       const response = await fetch("https://wish-dl8h.onrender.com/wish", {
-       method: "POST",
-       headers: {
-      "Content-Type": "application/json",
-        },
-     
-          body: JSON.stringify({
-          taggedUserID: taggedUserID,
-          taggedUserName: taggedUserName,
-          threadID: event.threadID,
-          messageID: event.messageID,
-        }),
-      });
-
+      const response = await fetch(`https://mahmud-wish.onrender.com/wish/font1?taggedUserName=${taggedUserName}`);
       const data = await response.json();
-
-       if (data.status === "success") {
-       const message = data.response;
-       await api.sendMessage(message, event.threadID, event.messageID);
+      if (data.status === "success") {
+        api.sendMessage(data.response, event.threadID, event.messageID);
       } else {
-        api.sendMessage(
-          "❌ Failed to send the birthday wish. Please try again later.",
-          event.threadID,
-          event.messageID
-        );
+        api.sendMessage("❌ Failed to send the birthday wish. Please try again later.", event.threadID, event.messageID);
       }
     } catch (err) {
-      console.error("❌ Failed to send message:", err);
-      api.sendMessage(
-      "❌ Something went wrong while sending the birthday wish.",
-      event.threadID,
-      event.messageID
-      );
+      api.sendMessage(`❌ Something went wrong: ${err.message}`, event.threadID, event.messageID);
     }
   },
 };
