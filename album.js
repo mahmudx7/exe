37ee5e7be9c7bcfcf9dbc04002920bbf466fe507
Mah MUD <mahmudx077@gmail.com>
2025-04-09 17:26:59 +0600

    Update album.js

--- album.js
+++ album.js
@@ -111,9 +111,9 @@ module.exports = {
       const displayedCategories = categories.slice(startIndex, endIndex);
 
       const message = `𝐀𝐯𝐚𝐢𝐥𝐚𝐛𝐥𝐞 𝐀𝐥𝐛𝐮𝐦 𝐕𝐢𝐝𝐞𝐨 𝐋𝐢𝐬𝐭 🎀\n` +
-        "━━━━━━━━━━━━━━━━━━━━━\n" +
+        "𐙚━━━━━━━━━━━━━━━━━━━━━ᡣ𐭩\n" +
         displayedCategories.map((option, index) => `${startIndex + index + 1}. ${option}`).join("\n") +
-        "\n━━━━━━━━━━━━━━━━━━━━━" +
+        "\n𐙚━━━━━━━━━━━━━━━━━━━━━ᡣ𐭩" +
         `\n♻ | 𝐏𝐚𝐠𝐞 [${page}/${totalPages}]<😘\nℹ | 𝐓𝐲𝐩𝐞 !album ${page + 1} - 𝐭𝐨 𝐬𝐞𝐞 𝐧𝐞𝐱𝐭 𝐩𝐚𝐠𝐞.`.repeat(page < totalPages);
 
         await api.sendMessage(message, event.threadID, (error, info) => {
