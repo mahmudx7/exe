b3445aaac73a396a76331753d1a9886d0320ac7c
Mah MUD <mahmudx077@gmail.com>
2025-04-13 03:03:55 +0600

    Update anisr.js

--- anisr.js
+++ anisr.js
@@ -1,7 +1,7 @@
 const axios = require('axios');
 
 module.exports = {
-  config: {
+    config: {
     name: "anisr",
     aliases: ["tiksr", "tiktoksr"],
     author: "MahMUD",
@@ -11,16 +11,16 @@ module.exports = {
   },
   onStart: async function ({ api, event, args }) {
     async function fetchTikTokVideos(query) {
-      try {
-        const response = await axios.post(
-          'https://mahmud-anisr.onrender.com/api/tiktok-search', 
-          { query },
-          { headers: { "author": module.exports.config.author } }
-        );
-        return response.data; 
-      } catch (error) {
-        console.error('Error fetching TikTok videos:', error);
-        return null;
+    try {
+    const response = await axios.post(
+   'https://mahmud-anisr-api.onrender.com/api/anisr/vid', 
+    { query },
+    { headers: { "author": module.exports.config.author } }
+     );
+    return response.data; 
+  } catch (error) {
+    console.error('Error🥹', error);
+    return null;
       }
     }
 
@@ -28,30 +28,30 @@ module.exports = {
     const query = args.join(' ');
 
     if (!query) {
-      api.sendMessage({ body: "Please provide a search query." }, event.threadID, event.messageID);
-      return;
+    api.sendMessage({ body: "Please provide a search query." }, event.threadID, event.messageID);
+    return;
     }
 
     const modifiedQuery = `${query} anime edit`;
     const response = await fetchTikTokVideos(modifiedQuery);
 
     if (!response || !response.videoUrl) {
-      api.sendMessage({ body: `No video found for query: ${query}.` }, event.threadID, event.messageID);
-      return;
+    api.sendMessage({ body: `No video found for query: ${query}.` }, event.threadID, event.messageID);
+    return;
     }
 
     const title = response.title || "No title available";
     const videoUrl = response.videoUrl;
 
     try {
-      const videoStream = await axios.get(videoUrl, { responseType: 'stream' });
-      api.sendMessage({
-        body: `𝐍𝐚𝐰 𝐛𝐚𝐛𝐲 𝐞𝐝𝐢𝐭𝐳 𝐯𝐢𝐝𝐞𝐨 <😘`,
-        attachment: videoStream.data,
-      }, event.threadID, event.messageID);
-    } catch (error) {
-      console.error(error);
-      api.sendMessage({ body: 'An error occurred while processing the video.\nPlease try again later.' }, event.threadID, event.messageID);
+    const videoStream = await axios.get(videoUrl, { responseType: 'stream' });
+    api.sendMessage({
+    body: `𝐍𝐚𝐰 𝐛𝐚𝐛𝐲 𝐞𝐝𝐢𝐭𝐳 𝐯𝐢𝐝𝐞𝐨 <😘`,
+    attachment: videoStream.data,
+    }, event.threadID, event.messageID);
+   } catch (error) {
+    console.error(error);
+    api.sendMessage({ body: 'error Please try again later.' }, event.threadID, event.messageID);
     }
   },
 };
