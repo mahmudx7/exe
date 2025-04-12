4bae4e4288f824f5feca780ab203ba73ab7cbc14
Mah MUD <mahmudx077@gmail.com>
2025-04-12 17:40:33 +0600

    Update gemini.js

--- gemini.js
+++ gemini.js
@@ -1,7 +1,7 @@
 const axios = require("axios");
 
 const baseApiUrl = async () => {
-  const base = 'https://mahmud-geminii.onrender.com';
+  const base = 'https://mahmud-gemini-api.onrender.com';
   return base;
 };
 
