df1d45a45aed3f33c60add7d130e15b058bf067a
Mah MUD <mahmudx077@gmail.com>
2025-04-19 12:57:37 +0600

    Update time.js

--- time.js
+++ time.js
@@ -1,44 +1,36 @@
 const axios = require('axios');
 
+const baseApiUrl = async () => {
+  const base = await axios.get("https://raw.githubusercontent.com/mahmudx7/exe/main/baseApiUrl.json");
+  return base.data.mahmud;
+};
+
 module.exports = {
   config: {
     name: "time",
     version: "1.7",
     author: "MahMUD",
-    countDown: 2,
+    countDown: 5,
     role: 0,
-    longDescription: "Fetch the current time in different countries.",
     category: "utility",
     guide: "{pn} [country] | {pn} list\n\nExamples:\n{pn} bangladesh\n{pn} london\n{pn} list"
   },
 
   onStart: async function ({ message, args }) {
-    let country = args[0]?.toLowerCase() || "bangladesh";
-
-    if (country === 'list') {
-      try {
-        const response = await axios.get('https://mahmud-time.onrender.com/time/list', {
-          headers: { "author": module.exports.config.author }
-        });
-
-        return response.data.message 
-          ? message.reply(response.data.message) 
-          : message.reply("⚠️ Unable to fetch country list.");
-      } catch (error) {
-        return message.reply("⚠️ An error occurred. Please try again later.");
-      }
-    }
+    const country = args[0]?.toLowerCase() || "bangladesh";
 
     try {
-      const response = await axios.get(`https://mahmud-time-api.onrender.com/time/${country}`, {
-        headers: { "author": module.exports.config.author }
+      const baseUrl = await baseApiUrl();
+
+      const response = await axios.get(`${baseUrl}/time/${country}`, {
+      headers: { "author": module.exports.config.author }
       });
 
-      return response.data.error 
-        ? message.reply(response.data.error) 
-        : message.reply(response.data.message);
+      return response.data.message
+     ? message.reply(response.data.message)
+     : message.reply("⚠️ Unable to fetch time.");
     } catch (error) {
-      return message.reply("⚠️ An error occurred. Please try again later.");
+      return message.reply("An error occurred. Please try again later.");
     }
   }
 };
