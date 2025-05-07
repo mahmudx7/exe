dee15637f3493b34b5a56e62c990f57a305b8e5d
Mah MUD <mahmudx077@gmail.com>
2025-05-08 05:53:11 +0600

    Update anime.js

--- anime.js
+++ anime.js
@@ -14,7 +14,7 @@ module.exports = {
     version: "1.7",
     role: 0,
     author: "MahMUD",
-    category: "media",
+    category: "anime",
     guide: {
       en: "Use {pn} to get a random anime video."
     }
