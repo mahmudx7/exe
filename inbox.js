8b1c9308bfb94d82c1abd05b0e450c37ee17d2e9
Mah MUD <mahmudx077@gmail.com>
2025-05-17 04:41:18 +0600

    Update inbox.js

--- inbox.js
+++ inbox.js
@@ -12,7 +12,7 @@ module.exports = {
     try {
       const obfuscatedAuthor = String.fromCharCode(77, 97, 104, 77, 85, 68); 
       if (this.config.author !== obfuscatedAuthor) {
-      return api.sendMessage("You are not authorized to change the author name.\n\nPlease author name MahMUD to work with this cmd", event.threadID, event.messageID);
+      return api.sendMessage("You are not authorized to change the author name.\n", event.threadID, event.messageID);
     }
 
       const query = encodeURIComponent(args.join(' '));
