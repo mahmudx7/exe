dc7454f19056b769dd2d2e9c08c4c19035697c20
Mah M UD <mahmudx077@gmail.com>
2025-10-13 04:37:15 +0600

    Update car.js

--- car.js
+++ car.js
@@ -28,7 +28,7 @@ module.exports = {
     
     try {
       const apiUrl = await mahmud();
-      const res = await axios.get(`${apiUrl}/api/album/videos/bike?userID=${event.senderID}`);
+      const res = await axios.get(`${apiUrl}/api/album/videos/car?userID=${event.senderID}`);
       if (!res.data.success || !res.data.videos.length)
         return api.sendMessage("❌ | No videos found.", event.threadID, event.messageID);
 
