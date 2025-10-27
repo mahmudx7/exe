4293baf61b7567bdedd0943dce8de6db4eab0d07
Mah M UD <mahmudx077@gmail.com>
2025-10-27 11:42:48 +0600

    Update and rename sing.js to song.js

--- song.js
+++ song.js
@@ -7,7 +7,7 @@ const mahmud = async () => {
 
 module.exports = {
     config: {
-        name: "sing",
+        name: "song",
         version: "1.7",
         author: "MahMUD", 
         countDown: 10,
