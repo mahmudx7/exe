6690cac0abe0f5a2c127ba904a62b769ec8d2b52
Mah M UD <mahmudx077@gmail.com>
2025-11-21 02:34:52 +0600

    Update myqueen.js

--- myqueen.js
+++ myqueen.js
@@ -17,54 +17,62 @@ const baseApiUrl = async () => {
 module.exports = {
   config: {
     name: "myqueen",
-    version: "1.0",
+    version: "1.1",
     author: "MahMUD",
     category: "love",
-    guide: "{pn} @mention",
+    guide: "{pn} @mention | reply | UID",
   },
 
-  onStart: async function ({ api, usersData, event }) {
+  onStart: async function ({ api, usersData, event, args }) {
     const obfuscatedAuthor = String.fromCharCode(77, 97, 104, 77, 85, 68); 
     if (module.exports.config.author !== obfuscatedAuthor) {
       return api.sendMessage("You are not authorized to change the author name.\n", event.threadID, event.messageID);
     }
     const senderID = event.senderID;
-    const mention = Object.keys(event.mentions)[0];
+    let target;
 
-    if (!mention) {
-      return api.sendMessage("❌ Mention someone to make them your Queen!", event.threadID, event.messageID);
-    }
+    const mention = Object.keys(event.mentions)[0];
+    if (mention) target = mention;
+    else if (event.messageReply) target = event.messageReply.senderID;
+    else if (args[0] && /^\d+$/.test(args[0])) target = args[0];
+
+    if (!target)
+      return api.sendMessage(
+        "❌ Mention, reply, or give UID to make someone your Queen!",
+        event.threadID,
+        event.messageID
+      );
 
     const user1 = senderID;   
-    const user2 = mention;     
+    const user2 = target;     
 
-    const data1 = await usersData.get(user1);
-    const data2 = await usersData.get(user2);
-    const name1 = data1.name;
-    const name2 = data2.name;
+    const info1 = await usersData.get(user1);
+    const info2 = await usersData.get(user2);
+
+    const name1 = info1.name;
+    const name2 = info2.name;
 
     try {
       const apiUrl = await baseApiUrl();
       const { data } = await axios.get(
-        `${apiUrl}/api/friend?user1=${user1}&user2=${user2}&style=2`,
+        `${apiUrl}/api/pair?user1=${user1}&user2=${user2}&style=26`,
         { responseType: "arraybuffer" }
       );
 
-      const filePath = path.join(__dirname, `myqueen_${senderID}.png`);
-      fs.writeFileSync(filePath, Buffer.from(data));
+      const file = path.join(__dirname, `myqueen_${senderID}.png`);
+      fs.writeFileSync(file, Buffer.from(data));
 
       api.sendMessage(
         {
           body: `𝐐𝐮𝐞𝐞𝐧 𝐨𝐟 𝐦𝐲 𝐡𝐞𝐚𝐫𝐭, 𝐫𝐮𝐥𝐞𝐫 𝐨𝐟 𝐦𝐲 𝐰𝐨𝐫𝐥𝐝 👸\n• ${name1}\n• ${name2}`,
-          attachment: fs.createReadStream(filePath),
+          attachment: fs.createReadStream(file),
         },
         event.threadID,
-        () => fs.unlinkSync(filePath),
+        () => fs.unlinkSync(file),
         event.messageID
       );
-
-    } catch (e) {
-      api.sendMessage("🥹error, contact MahMUD." + e.message, event.threadID, event.messageID);
+    } catch (err) {
+      api.sendMessage("🥹error, contact MahMUD." + err.message, event.threadID, event.messageID);
     }
   },
 };
