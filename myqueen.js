62254c18ff66e095f787695c0fe01b331b993133
Mah M UD <mahmudx077@gmail.com>
2025-11-21 02:30:49 +0600

    Create myqueen.js

---
+++ myqueen.js
@@ -0,0 +1,61 @@
+const axios = require("axios");
+const fs = require("fs-extra");
+const path = require("path");
+
+const baseApiUrl = async () => {
+  const base = await axios.get(
+    "https://raw.githubusercontent.com/mahmudx7/exe/main/baseApiUrl.json"
+  );
+  return base.data.mahmud;
+};
+
+module.exports = {
+  config: {
+    name: "myqueen",
+    version: "1.0",
+    author: "MahMUD",
+    category: "love",
+    guide: "{pn} @mention",
+  },
+
+  onStart: async function ({ api, usersData, event }) {
+    const senderID = event.senderID;
+    const mention = Object.keys(event.mentions)[0];
+
+    if (!mention) {
+      return api.sendMessage("❌ Mention someone to make them your Queen!", event.threadID, event.messageID);
+    }
+
+    const user1 = senderID;   
+    const user2 = mention;     
+
+    const data1 = await usersData.get(user1);
+    const data2 = await usersData.get(user2);
+    const name1 = data1.name;
+    const name2 = data2.name;
+
+    try {
+      const apiUrl = await baseApiUrl();
+      const { data } = await axios.get(
+        `${apiUrl}/api/friend?user1=${user1}&user2=${user2}&style=2`,
+        { responseType: "arraybuffer" }
+      );
+
+      const filePath = path.join(__dirname, `myqueen_${senderID}.png`);
+      fs.writeFileSync(filePath, Buffer.from(data));
+
+      api.sendMessage(
+        {
+          body: `𝐐𝐮𝐞𝐞𝐧 𝐨𝐟 𝐦𝐲 𝐡𝐞𝐚𝐫𝐭, 𝐫𝐮𝐥𝐞𝐫 𝐨𝐟 𝐦𝐲 𝐰𝐨𝐫𝐥𝐝 👸\n• ${name1}\n• ${name2}`,
+          attachment: fs.createReadStream(filePath),
+        },
+        event.threadID,
+        () => fs.unlinkSync(filePath),
+        event.messageID
+      );
+
+    } catch (e) {
+      api.sendMessage("❌ Error: " + e.message, event.threadID, event.messageID);
+    }
+  },
+};
