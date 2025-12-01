e708659d0e16c31b0a71a1c4987db7c411f9c9c0
Mah M UD <mahmudx077@gmail.com>
2025-12-01 21:45:54 +0600

    Update blur.js

--- blur.js
+++ blur.js
@@ -1,7 +1,7 @@
 const axios = require("axios");
 
 const mahmud = async () => {
-  const response = await axios.get("https://raw.githubusercontent.com/mahmudx7/exe/main/baseApiUrl.json");
+  const response = await axios.get("https://raw.githubusercontent.com/mahmudx7/HINATA/main/baseApiUrl.json");
   return response.data.mahmud;
 };
 
