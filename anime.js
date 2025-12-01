c51fbcd94696280b04e0c13a685facf7f98dd680
Mah M UD <mahmudx077@gmail.com>
2025-12-01 16:34:45 +0600

    Update anime.js

--- anime.js
+++ anime.js
@@ -3,7 +3,7 @@ const fs = require("fs");
 const path = require("path");
 
 const mahmud = async () => {
-  const response = await axios.get("https://raw.githubusercontent.com/mahmudx7/exe/main/baseApiUrl.json");
+  const response = await axios.get("https://raw.githubusercontent.com/mahmudx7/HINATA/main/baseApiUrl.json");
   return response.data.mahmud;
 };
 
