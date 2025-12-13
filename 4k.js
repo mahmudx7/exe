d9af0fce8b755fa3e3c1ef69c5ca4766d985c71a
Mah M UD <mahmudx077@gmail.com>
2025-12-14 03:53:25 +0600

    Update 4k.js

--- 4k.js
+++ 4k.js
@@ -25,49 +25,37 @@ module.exports = {
   },
 
   onStart: async function ({ message, event, args }) {
-    
-    const obfuscatedAuthor = String.fromCharCode(77, 97, 104, 77, 85, 68); 
-    if (module.exports.config.author !== obfuscatedAuthor) {
-      return api.sendMessage("You are not authorized to change the author name.", event.threadID, event.messageID);
+     const obfuscatedAuthor = String.fromCharCode(77, 97, 104, 77, 85, 68); 
+     if (module.exports.config.author !== obfuscatedAuthor) {
+     return api.sendMessage("You are not authorized to change the author name.", event.threadID, event.messageID);
     }
-    const startTime = Date.now();
-    let imgUrl;
-
-    if (event.messageReply?.attachments?.[0]?.type === "photo") {
-      imgUrl = event.messageReply.attachments[0].url;
+     const startTime = Date.now();
+     let imgUrl;
+     if (event.messageReply?.attachments?.[0]?.type === "photo") {
+     imgUrl = event.messageReply.attachments[0].url;
     }
 
-    else if (args[0]) {
-      imgUrl = args.join(" ");
+     else if (args[0]) {
+     imgUrl = args.join(" ");
     }
 
-    if (!imgUrl) {
-      return message.reply("Baby, Please reply to an image or provide an image URL");
-    }
-  
-    const waitMsg = await message.reply("𝐋𝐨𝐚𝐝𝐢𝐧𝐠 𝟒𝐤 𝐢𝐦𝐚𝐠𝐞...𝐰𝐚𝐢𝐭 𝐛𝐚𝐛𝐲 <😘");
-    message.reaction("😘", event.messageID);
+     if (!imgUrl) { return message.reply("Baby, Please reply to an image or provide an image URL"); }
+     const waitMsg = await message.reply("𝐋𝐨𝐚𝐝𝐢𝐧𝐠 𝟒𝐤 𝐢𝐦𝐚𝐠𝐞...𝐰𝐚𝐢𝐭 𝐛𝐚𝐛𝐲 <😘");
+     message.reaction("😘", event.messageID);
 
     try {
-      
-      const apiUrl = `${await mahmud()}/api/hd?imgUrl=${encodeURIComponent(imgUrl)}`;
+     const apiUrl = `${await mahmud()}/api/hd?imgUrl=${encodeURIComponent(imgUrl)}`;
+     const res = await axios.get(apiUrl, { responseType: "stream" });
+     if (waitMsg?.messageID) message.unsend(waitMsg.messageID); message.reaction("✅", event.messageID);
+     const processTime = ((Date.now() - startTime) / 1000).toFixed(2);
 
-      const res = await axios.get(apiUrl, { responseType: "stream" });
-      if (waitMsg?.messageID) message.unsend(waitMsg.messageID);
-
-      message.reaction("✅", event.messageID);
-
-      const processTime = ((Date.now() - startTime) / 1000).toFixed(2);
-
-      message.reply({
+     message.reply({
         body: `✅ | 𝐇𝐞𝐫𝐞'𝐬 𝐲𝐨𝐮𝐫 𝟒𝐤 𝐢𝐦𝐚𝐠𝐞 𝐛𝐚𝐛𝐲`,
         attachment: res.data
       });
 
-    } catch (error) {
-  
+  } catch (error) {
       if (waitMsg?.messageID) message.unsend(waitMsg.messageID);
-
       message.reaction("❎", event.messageID);
       message.reply(`🥹error baby, contact MahMUD.`);
     }
